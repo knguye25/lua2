433f7aaab6b8693b3df134057b79f1cc3bf92abb
Khanh Nguyen <Khanh@Khanhs-MacBook-Air.local>
2016-03-28 00:25:36 -0400

    added movement to buttons

--- main.lua
+++ main.lua
@@ -10,9 +10,7 @@ physics.start()
 display.setDefault("background", 129/500, 0, 0)
 
 -- setup gravity
-local gravityx, gravityy = physics.getGravity()
-physics.setGravity( gravityx, gravityy)
-
+physics.setGravity( 0, 0)
 local dusk = require("Dusk.Dusk")
 local map = dusk.buildMap("level.json")
 local layer = map.layer[1]
@@ -33,22 +31,22 @@ map:addEventListener("touch", map)
 --]=====]
 -- setup wall tiles
 for tile in map.layer[1].tilesInRange(1, 1, 1, 10) do
-  physics.addBody( tile, "static", {density =1.0, friction = 0.3, bounce =0.1})
+  physics.addBody( tile, "static", {density =0, friction = 0, bounce =0})
   tile.id = "tile"
 end
 
 for tile in map.layer[1].tilesInRange(8, 1, 1, 10) do
-  physics.addBody( tile, "static", {density =1.0, friction = 0.3, bounce =0.1})
+  physics.addBody( tile, "static", {density =0, friction = 0, bounce =0})
   tile.id = "tile"
 end
 
-for tile in map.layer[1].tilesInRange(2, 1, 7, 1) do
-  physics.addBody( tile, "static", {density =1.0, friction = 0.3, bounce =0.1})
+for tile in map.layer[1].tilesInRange(2, 1, 6, 1) do
+  physics.addBody( tile, "static", {density =0, friction = 0, bounce =0})
   tile.id = "tile"
 end
 
 for tile in map.layer[1].tilesInRange(2, 10, 7, 10) do
-  physics.addBody( tile, "static", {density =1.0, friction = 0.3, bounce =0.1})
+  physics.addBody( tile, "static", {density =0, friction = 0, bounce =0})
   tile.id = "tile"
 end
 
@@ -72,28 +70,11 @@ end
 -- start enemy movement
 moveEnemy()
 
--- setup button controls
-local function onObjectTouch( self, event )
-    if ( event.phase == "began" ) then
-        print( "Touch event began on: " .. self.id )
-    end
-    return true
-end
-
-local buttons = {map.layer[1].tile(9, 5), map.layer[1].tile(11, 5), map.layer[1].tile(10, 4), map.layer[1].tile(10, 6)}
-buttons[1].id = "leftBtn"
-buttons[2].id = "rightBtn"
-buttons[3].id = "upBtn"
-buttons[4].id = "downBtn"
-
-for buttonIdex=1, 4, 1 do
-  buttons[buttonIdex].touch = onObjectTouch
-  buttons[buttonIdex]:addEventListener( "touch", buttons[buttonIdex] )
-end
 -- setup player
 local player = map.layer[1].tile(2, 2)
+player.y = player.y+1
 player.id = "player"
-physics.addBody( player, "dynamic", {density =1.0, friction = 0.5, bounce =0})
+physics.addBody( player, "dynamic", {density =0, friction = 0, bounce =0})
 player.isFixedRotation = true
 
 -- make player dragable
@@ -109,3 +90,34 @@ function player:touch(event)
   return true
 end
 player:addEventListener("touch", player)
+
+
+-- setup button controls
+local buttons = {map.layer[1].tile(9, 5), map.layer[1].tile(11, 5), map.layer[1].tile(10, 4), map.layer[1].tile(10, 6)}
+buttons[1].id = "leftBtn"
+buttons[2].id = "rightBtn"
+buttons[3].id = "upBtn"
+buttons[4].id = "downBtn"
+
+function onObjectTouch( self, event )
+    if ( event.phase == "began" ) then
+        print( "Touch event began on: " .. self.id )
+        initialId =self.id
+      elseif(event.phase == "ended") then
+        if(self.id == "leftBtn" and initialId == self.id) then
+          player:setLinearVelocity( -100, 0)
+        elseif(self.id == "rightBtn" and initialId == self.id) then
+          player:setLinearVelocity(100, 0)
+        elseif(self.id == "upBtn" and initialId == self.id) then
+          player:setLinearVelocity(0, -100)
+        elseif(self.id == "downBtn" and initialId == self.id) then
+          player:setLinearVelocity(0, 100)
+        end
+    end
+    return true
+end
+
+for buttonIdex=1, 4, 1 do
+  buttons[buttonIdex].touch = onObjectTouch
+  buttons[buttonIdex]:addEventListener( "touch", buttons[buttonIdex] )
+end
